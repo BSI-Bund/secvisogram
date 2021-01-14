eb76013ef4dbb0a77ef1a18f8b4cf01ba851f7cc
Dominik Burgdörfer <db@burgdoerfer.com>
2021-01-14 10:52:46 +0100

    Enable switching between tab-components

--- viewTests/shared/suites/SecvisogramPage.js
+++ viewTests/shared/suites/SecvisogramPage.js
@@ -16,15 +16,11 @@ const props = {
     },
   },
   activeTab: /** @type {'EDITOR'} */ ('EDITOR'),
-  editorPageUrl: '#/editor',
-  sourcePageUrl: '#/source',
-  advisoryPageUrl: '#/advisory',
-  htmlPageUrl: '#/html',
-  csafJsonPageUrl: '#/csafJson',
   onNew: console.log.bind(console, 'onNew'),
   onDownload: console.log.bind(console, 'onDownload'),
   onOpen: console.log.bind(console, 'onOpen'),
   onSave: console.log.bind(console, 'onSave'),
+  onChangeTab: console.log.bind(console, 'onChangeTab'),
 }
 
 export const tests = [
