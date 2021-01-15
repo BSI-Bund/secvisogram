e29a4eea2fcddaa01d19b02ca1772315187a7c1c
Dominik Burgdörfer <db@burgdoerfer.com>
2021-01-15 15:00:58 +0100

    Cleanup core

--- tests/Core.js
+++ tests/Core.js
@@ -4,23 +4,6 @@ import createCoreFixture from './shared/CoreFixture'
 
 suite('Core', () => {
   suite('DocumentService', () => {
-    test('The document can be loaded for editing', async () => {
-      const coreFixture = await createCoreFixture()
-      const core = await createCore(coreFixture.params)
-
-      const result = await core.document.edit()
-
-      expect(result).to.be.ok
-    })
-
-    test('The document can be saved', async () => {
-      const coreFixture = await createCoreFixture()
-      const core = await createCore(coreFixture.params)
-      const doc = { ...coreFixture.document, foo: 'bar' }
-
-      await core.document.save(doc)
-    })
-
     test('The document can be validated against the JSON-schema', async () => {
       const coreFixture = await createCoreFixture()
       const core = await createCore(coreFixture.params)
