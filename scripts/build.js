c284af6d483a1e901b0379c7c701f53a1464d1e7
Dominik Burgdörfer <db@burgdoerfer.com>
2021-01-15 15:41:05 +0100

    Perform build using node-script for windows-compatibility

---
+++ scripts/build.js
@@ -0,0 +1,12 @@
+const { spawn } = require('child_process')
+
+spawn('webpack --mode production', {
+  env: {
+    ...process.env,
+    NODE_ENV: 'production',
+  },
+  stdio: 'inherit',
+  shell: true,
+}).on('exit', (code) => {
+  process.exit(code ?? 0)
+})
