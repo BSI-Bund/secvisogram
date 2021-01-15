f8ec6b82220b20d63cf0ca6550abd6a2bffa7d2b
Dominik Burgdörfer <db@burgdoerfer.com>
2021-01-15 14:54:03 +0100

    Add core-method to validate document

--- tests/Core.js
+++ tests/Core.js
@@ -20,5 +20,22 @@ suite('Core', () => {
 
       await core.document.save(doc)
     })
+
+    test('The document can be validated against the JSON-schema', async () => {
+      const coreFixture = await createCoreFixture()
+      const core = await createCore(coreFixture.params)
+
+      for (const document of coreFixture.documents) {
+        const result = await core.document.validate({
+          document: document.content,
+        })
+        expect(result.isValid).to.equal(document.valid)
+        if (document.valid) {
+          expect(result.errors).to.have.lengthOf(0)
+        } else {
+          expect(result.errors).have.length.greaterThan(0)
+        }
+      }
+    })
   })
 })
--- tests/shared/CoreFixture.js
+++ tests/shared/CoreFixture.js
@@ -1,7 +1,15 @@
+import valid1 from '../../seeds/documents/valid-1.json'
+import valid2 from '../../seeds/documents/valid-2.json'
+
 export default async function createCoreFixture() {
   return {
     params: {},
 
     document: {},
+    documents: [
+      { valid: true, content: valid1 },
+      { valid: true, content: valid2 },
+      { valid: false, content: {} },
+    ],
   }
 }
