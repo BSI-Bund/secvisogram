af41b94aaeff1cbf62e20d5d290c81e9846ead54
Dominik Burgdörfer <db@burgdoerfer.com>
2021-01-18 15:11:52 +0100

    Add sample-views for "HTML" and "Advisory"

--- viewTests/shared/suites.js
+++ viewTests/shared/suites.js
@@ -1,6 +1,6 @@
 import '../../lib/shared/style.css'
 import * as ErrorScreen from './suites/ErrorScreen'
 import * as LoadingIndicator from './suites/LoadingIndicator'
-import * as EditorPage from './suites/SecvisogramPage'
+import * as SecvisogramPage from './suites/SecvisogramPage'
 
-export default [EditorPage, ErrorScreen, LoadingIndicator]
+export default [SecvisogramPage, ErrorScreen, LoadingIndicator]
