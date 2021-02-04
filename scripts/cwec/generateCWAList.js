cc741e80d8500bf43c8f35c342555cca1ed294fb
Till Husemann <till.husemann@exxcellent.de>
2021-02-04 17:04:55 +0100

    Generate cwec weaknesses via script

---
+++ scripts/cwec/generateCWAList.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const xml2js = require('xml2js')
+
+/**
+ * @typedef {{ ID: string; Name: string }} Weakness
+ * @typedef {{Weaknesses: {Weakness: Array<Weakness>}}} Weaknesses
+ */
+
+function generateCWAList() {
+  const parser = new xml2js.Parser({
+    explicitArray: false,
+    mergeAttrs: true,
+    explicitRoot: false,
+  })
+  fs.readFile(__dirname + '/cwec_v4.3.xml', function (...args) {
+    parser.parseStringPromise(args[1]).then(function (result) {
+      console.log('{ "weaknesses": [')
+      result.Weaknesses.Weakness.map((/** @type {Weakness} */ weakness) => {
+        console.log(`{"id": "${weakness.ID}", "name": "${weakness.Name}"},`)
+      })
+      console.log(']}')
+    })
+  })
+}
+
+generateCWAList()
