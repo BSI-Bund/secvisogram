330c6947f0a1f79d0f1a6f0a5d2bc5a10c645c03
Dominik Burgdörfer <db@burgdoerfer.com>
2021-02-08 17:05:06 +0100

    Regenerate vector-string when changing vector-values

--- tests/SecvisogramPage.js
+++ tests/SecvisogramPage.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import CVSSVector from '../lib/SecvisogramPage/View/EditorTab/Vulnerabilities/Scores/CVSS3Editor/CVSSVector'
 import ViewReducer from '../lib/SecvisogramPage/View/Reducer'
 
 suite('SecvisogramPage', () => {
@@ -13,6 +14,19 @@ suite('SecvisogramPage', () => {
         })
 
         expect(state.formValues.doc).to.equal(42)
+      })
+
+      test('The document can be updated using a data-path', () => {
+        let { state } = Fixture()
+        state.formValues.doc = { foobar: {} }
+
+        state = ViewReducer(state, {
+          type: 'CHANGE_FORM_DOC',
+          dataPath: '/foobar/test',
+          update: { $set: 42 },
+        })
+
+        expect(state.formValues.doc.foobar.test).to.equal(42)
       })
 
       test('The form can be reset', () => {
@@ -38,6 +52,18 @@ suite('SecvisogramPage', () => {
           },
         }
       }
+    })
+
+    suite('CVSSVector', () => {
+      test('A vector-string can be generated', () => {
+        const vector = new CVSSVector({ attackVector: 'NETWORK' })
+          .set('attackComplexity', 'LOW')
+          .set('privilegesRequired', 'NONE')
+          .remove('privilegesRequired')
+          .set('reportConfidence', 'NOT_DEFINED')
+
+        expect(vector.toString()).to.equal('CVSS:3.1/AV:N/AC:L/RC:X')
+      })
     })
   })
 })
