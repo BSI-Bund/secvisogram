ea33a1ebeefc32a9fbd2b2787ffb87c6f98690c9
Dominik Burgdörfer <db@burgdoerfer.com>
2021-02-10 15:35:54 +0100

    Load vendor module during test-run

--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,15 @@
 const { spawn } = require('child_process')
 
-spawn('npx electron-mocha -r @babel/register -u tdd --renderer tests/index.js', {
-  env: {
-    ...process.env,
-    BABEL_ENV: 'test',
-  },
-  stdio: 'inherit',
-  shell: true,
-}).on('exit', (code) => {
+spawn(
+  'npx electron-mocha -r @babel/register -u tdd --script ./vendor/first/cvsscalc31.js --renderer tests/index.js',
+  {
+    env: {
+      ...process.env,
+      BABEL_ENV: 'test',
+    },
+    stdio: 'inherit',
+    shell: true,
+  }
+).on('exit', (code) => {
   process.exit(code ?? 0)
 })
