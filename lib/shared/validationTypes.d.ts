7ea9d3b653815ff120f5b0d959f7f6fedbc977e4
Dominik Burgdörfer <db@burgdoerfer.com>
2021-02-16 14:29:13 +0100

    Remove unnecessary type-definition

--- lib/shared/validationTypes.d.ts
+++ lib/shared/validationTypes.d.ts
@@ -2,8 +2,3 @@ export interface ValidationError {
   message: string
   dataPath: string
 }
-/**
- * @typedef {Object} ValidationError
- * @property {string} message
- * @property {string} dataPath
- */
