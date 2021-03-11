89a5c330e5c8f8b587e1c0a2b42bce1c5cbf1fac
Till Husemann <till.husemann@exxcellent.de>
2021-03-11 18:33:14 +0100

    Replace datalist with material-ui/Autocomplete

--- app/lib/SecvisogramPage/View/FormEditorTab/shared/EnumAttribute.js
+++ app/lib/SecvisogramPage/View/FormEditorTab/shared/EnumAttribute.js
@@ -1,3 +1,5 @@
+import { TextField } from '@material-ui/core'
+import { Autocomplete } from '@material-ui/lab'
 import '@reach/combobox/styles.css'
 import React from 'react'
 import Attribute from './shared/Attribute'
@@ -17,33 +19,34 @@ import Delete from './shared/Delete'
  *  onUpdate(dataPath: string, update: {}): void
  * }} props
  */
-export default function EnumAttribute({
-  options,
-  required = true,
-  deletable,
-  ...props
-}) {
-  const listId = `${props.dataPath}-list`
+export default function EnumAttribute({ options, deletable, ...props }) {
+  const [inputValue, setInputValue] = React.useState(props.value)
   return (
     <Attribute {...props}>
       {({ onChange, onDelete }) => (
         <div className="max-w-md flex">
           <div className="w-full">
-            <input
-              className="border border-gray-400 py-1 px-2 w-full shadow-inner rounded"
-              type="text"
-              list={listId}
-              value={/** @type {string} */ (props.value)}
-              onChange={(e) => {
-                onChange(e.target.value, props.value)
+            <Autocomplete
+              freeSolo
+              disableClearable
+              options={options}
+              value={props.value}
+              onChange={(event, newValue) => {
+                onChange(newValue ?? '', props.value)
               }}
-              required={required}
+              inputValue={/** @type {string} */ (inputValue)}
+              onInputChange={(event, newInputValue) => {
+                setInputValue(newInputValue ?? '')
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  error={!props.value}
+                  margin="dense"
+                  variant="outlined"
+                />
+              )}
             />
-            <datalist id={listId}>
-              {options.map((option, index) => (
-                <option key={index} value={option} />
-              ))}
-            </datalist>
           </div>
           {deletable ? (
             <Delete
