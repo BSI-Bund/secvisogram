cfcb0e75b50f6dfabc4ca078497997aafda155a5
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2021-03-13 09:18:01 +0100

    build(Babel): pin compact per environment

--- app/babel.config.js
+++ app/babel.config.js
@@ -5,18 +5,21 @@ module.exports = {
         ['@babel/preset-env', { modules: false, targets: { esmodules: true } }],
         '@babel/preset-react',
       ],
+      compact: false,
     },
     test: {
       presets: [
         ['@babel/preset-env', { targets: { node: 'current' } }],
         '@babel/preset-react',
       ],
+      compact: true,
     },
     production: {
       presets: [
         ['@babel/preset-env', { modules: false, targets: { esmodules: true } }],
         '@babel/preset-react',
       ],
+      compact: true,
     },
   },
 }
