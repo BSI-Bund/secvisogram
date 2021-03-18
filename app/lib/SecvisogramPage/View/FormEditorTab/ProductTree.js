89ea29ce68a6fdf909d273fca2b662d48fd5d5aa
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2021-03-18 18:05:38 +0100

    fix(ProductTree): reenable deletion of product ids
    closes #59

--- app/lib/SecvisogramPage/View/FormEditorTab/ProductTree.js
+++ app/lib/SecvisogramPage/View/FormEditorTab/ProductTree.js
@@ -110,6 +110,7 @@ const ProductGroups = React.memo(
                       label="Reference token for product instance"
                       description="Token required to identify a full_product_name so that it can be referred to from other parts in the document. There is no predefined or required format for the product_id as long as it uniquely identifies a product in the context of the current document."
                       placeholder="CSAFPID-0004 ..."
+                      deletable
                       onCollectIds={onCollectProductIds}
                     />
                   )}
