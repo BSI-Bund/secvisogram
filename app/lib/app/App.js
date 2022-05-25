82778d79272d9c102ebd809f4cfa1e291efe3368
Christoph Löser <33527573+cloeser@users.noreply.github.com>
2022-05-25 15:39:50 +0200

    feat: add context to store app config

--- app/lib/app/App.js
+++ app/lib/app/App.js
@@ -3,6 +3,8 @@ import { ErrorBoundary } from 'react-error-boundary'
 import ErrorScreen from './App/ErrorScreen.js'
 import useHistory from './App/useHistory.js'
 import HistoryContext from './shared/HistoryContext.js'
+import ConfigContext from './shared/ConfigContext.js'
+import * as api from './shared/api.js'
 
 /**
  * @param {object} props
@@ -12,11 +14,21 @@ import HistoryContext from './shared/HistoryContext.js'
 export default function App({ secvisogramPage }) {
   const history = useHistory()
 
+  const [config, setConfig] = React.useState({});
+
+  React.useEffect(() => {
+    api.appConfig.getAppConfig().then(
+      response => setConfig(response)
+    )
+  }, []);
+
   return (
-    <HistoryContext.Provider value={history}>
-      <ErrorBoundary FallbackComponent={ErrorScreen}>
-        {secvisogramPage}
-      </ErrorBoundary>
-    </HistoryContext.Provider>
+    <ConfigContext.Provider value={config}>
+      <HistoryContext.Provider value={history}>
+        <ErrorBoundary FallbackComponent={ErrorScreen}>
+          {secvisogramPage}
+        </ErrorBoundary>
+      </HistoryContext.Provider>
+    </ConfigContext.Provider>
   )
 }
---
+++ app/lib/app/shared/ConfigContext.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const defaultValue = {
+  loginAvailable: false,
+}
+
+export default React.createContext(defaultValue)
