f002b72efd13245f247076d5fbada88f6a674cb7
Christoph Löser <33527573+cloeser@users.noreply.github.com>
2022-06-08 09:08:17 +0200

    feat: get config from fixed path instead of the current domain name

--- app/lib/app/shared/api/appConfig.js
+++ app/lib/app/shared/api/appConfig.js
@@ -1,20 +1,17 @@
-import APIRequest from "../APIRequest.js";
+import APIRequest from '../APIRequest.js'
 
 export async function getAppConfig() {
   try {
-    // .well-known spec: https://github.com/Vroo/well-known-uri-appspecific/blob/main/well-known-uri-for-application-specific-purposes.txt
-    // The config file should be named like the domain that the app is running on
-    let hostname = window.location.hostname;
-    let reversedHostname = hostname.split(".").reverse().join(".")
-
-    const res = await new APIRequest(new Request('.well-known/appspecific/' + reversedHostname + '.json'))
+    const response = await new APIRequest(
+      new Request('.well-known/appspecific/de.bsi.secvisogram.json')
+    )
       .produces('application/json')
       .send()
-    return await res.json()
+    return await response.json()
   } catch (error) {
-    console.info("Could not get config. Falling back to standalone mode.");
+    console.info('Could not get config. Falling back to standalone mode.')
     return {
-      loginAvailable: false
-    };
+      loginAvailable: false,
+    }
   }
 }
