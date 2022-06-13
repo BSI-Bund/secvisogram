b4a8e900973080c279da571742e32cc0e2336df7
FabianMathes <100683734+FabianMathes@users.noreply.github.com>
2022-06-13 16:06:05 +0200

    build: added monaco-editor dependencies

--- app/webpack.config.cjs
+++ app/webpack.config.cjs
@@ -4,6 +4,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 const Webpack = require('webpack')
 const gitRevisionPlugin = new GitRevisionPlugin()
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const path = require('path')
 
 /** @type {import('webpack').Configuration} */
@@ -76,6 +77,9 @@ module.exports = {
             template: './lib/index.html',
           }),
         ]),
+    new MonacoWebpackPlugin({
+      languages: ['json'],
+    }),
   ],
   devServer: {
     historyApiFallback: true,
