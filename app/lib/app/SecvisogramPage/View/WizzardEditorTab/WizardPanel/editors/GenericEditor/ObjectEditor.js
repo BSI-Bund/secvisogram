f73dc98acf68bbfaa2c026adc71cbe43e14cef3c
Dominik Burgdörfer <db@burgdoerfer.com>
2022-10-19 16:42:27 +0200

    fix: fix selection in object editor

--- app/lib/app/SecvisogramPage/View/WizzardEditorTab/WizardPanel/editors/GenericEditor/ObjectEditor.js
+++ app/lib/app/SecvisogramPage/View/WizzardEditorTab/WizardPanel/editors/GenericEditor/ObjectEditor.js
@@ -15,7 +15,6 @@ export default function ObjectEditor({
   instancePath,
 }) {
   const { selectedPath } = React.useContext(WizardContext)
-  const selectedSubPath = selectedPath.slice(instancePath.length)
   const fieldProperties = property.metaInfo.propertyList?.filter(
     (p) => !['OBJECT', 'ARRAY'].includes(p.type)
   )
@@ -35,16 +34,43 @@ export default function ObjectEditor({
       )
     }, /** @type {typeof property | null} */ (property))
 
-  const level = property.addMenuItemsForChildObjects ? 2 : 1
-  const selectedProperty = resolveSubProperty(selectedSubPath.slice(0, level))
+  const findSelectedProperty = () => {
+    const menuDepth = property.addMenuItemsForChildObjects ? 2 : 1
+    const selectedSubPath = selectedPath.slice(instancePath.length)
+
+    /** @type {import('../../shared/types').Property | null} */
+    let selectedProperty = null
+    /** @type {string[]} */
+    const sanitizedSelectedSubPath = []
+    for (const [i, pathSegment] of selectedSubPath.entries()) {
+      const nextProperty =
+        i + 1 > menuDepth
+          ? null
+          : resolveSubProperty(selectedSubPath.slice(0, i + 1))
+
+      // As soon as we find a property that is not an object or an array
+      // we reached the limit of the selectable property in this menu
+      if (
+        !nextProperty ||
+        (nextProperty.type !== 'OBJECT' && nextProperty.type !== 'ARRAY')
+      ) {
+        break
+      }
+      sanitizedSelectedSubPath.push(pathSegment)
+      selectedProperty = nextProperty
+    }
+    return { selectedProperty, selectedSubPath: sanitizedSelectedSubPath }
+  }
+
+  const { selectedProperty, selectedSubPath } = findSelectedProperty()
 
   const renderComplexEditor = () => {
     if (!selectedSubPath.length) return null
     if (!selectedProperty) return null
     return (
       <GenericEditor
-        key={instancePath.concat(selectedSubPath.slice(0, level)).join('.')}
-        instancePath={instancePath.concat(selectedSubPath.slice(0, level))}
+        key={instancePath.concat(selectedSubPath).join('.')}
+        instancePath={instancePath.concat(selectedSubPath)}
         parentProperty={resolveSubProperty(selectedSubPath.slice(0, -1))}
         property={selectedProperty}
       />
