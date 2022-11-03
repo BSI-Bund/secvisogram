c5e38f99e7a6beca068e8393896cf02b98242c04
vdeyringer <valentin.deyringer@exxcellent.de>
2022-11-03 18:17:43 +0100

    feat(SideBar): change sidebar expand/collaps functionality

--- app/lib/app/SecvisogramPage/View/SideBar/SideBar.js
+++ app/lib/app/SecvisogramPage/View/SideBar/SideBar.js
@@ -6,58 +6,48 @@ import {
   faExclamationTriangle,
 } from '@fortawesome/free-solid-svg-icons'
 import { faFileLines } from '@fortawesome/free-regular-svg-icons'
+import React from 'react'
+import SideBarContext from '../shared/context/SideBarContext.js'
 import ErrorPanel from './ErrorPanel.js'
 import InfoPanel from './InfoPanel.js'
 import CommentPanel from './CommentPanel.js'
-import React from 'react'
-import SideBarContext from '../shared/context/SideBarContext.js'
 
 export default function SideBar() {
   const sideBarData = React.useContext(SideBarContext)
 
   return (
-    <>
-      <div className="flex h-full w-3 justify-center items-center bg-gray-300 border-l border-black">
-        <button
-          onClick={() =>
-            sideBarData.setSideBarIsOpen(!sideBarData.sideBarIsOpen)
-          }
-        >
-          <FontAwesomeIcon
-            className="fa-xs"
-            icon={
-              sideBarData.sideBarIsOpen ? faAngleDoubleRight : faAngleDoubleLeft
-            }
-          />
-        </button>
-      </div>
-      {sideBarData.sideBarIsOpen ? (
-        <div className="h-full w-96 bg-gray-300 p-1.5">
-          <div className="flex justify-evenly bg-gray-300">
-            {[
-              { targetString: 'ERRORS', icon: faExclamationTriangle },
-              { targetString: 'COMMENTS', icon: faComment },
-              { targetString: 'DOCUMENTATION', icon: faFileLines },
-            ].map(({ targetString, icon }) => (
-              <div key={targetString} className="w-full">
-                <button
-                  data-testid={`sideBar-${targetString}-button`}
-                  className={
-                    'p-3 w-full ' +
-                    (sideBarData.sideBarContent === targetString
-                      ? 'bg-gray-200'
-                      : '')
+    <div className="flex flex-col bg-gray-300 border-l border-black">
+      <div className="flex h-full">
+        <div className="flex-col">
+          {[
+            { targetString: 'ERRORS', icon: faExclamationTriangle },
+            { targetString: 'COMMENTS', icon: faComment },
+            { targetString: 'DOCUMENTATION', icon: faFileLines },
+          ].map(({ targetString, icon }) => (
+            <div key={targetString}>
+              <button
+                data-testid={`sideBar-${targetString}-button`}
+                className={
+                  'p-3 w-full hover:bg-gray-200 ' +
+                  (sideBarData.sideBarIsOpen &&
+                  sideBarData.sideBarContent === targetString
+                    ? 'bg-gray-200'
+                    : '')
+                }
+                onClick={() => {
+                  sideBarData.setSideBarContent(targetString)
+                  if (!sideBarData.sideBarIsOpen) {
+                    sideBarData.setSideBarIsOpen(true)
                   }
-                  onClick={() => {
-                    sideBarData.setSideBarContent(targetString)
-                  }}
-                >
-                  <FontAwesomeIcon className="fa-2x" icon={icon} />
-                </button>
-              </div>
-            ))}
-          </div>
-          <div className="p-3 h-full w-full bg-gray-200 overflow-auto">
+                }}
+              >
+                <FontAwesomeIcon className="fa-2x" icon={icon} />
+              </button>
+            </div>
+          ))}
+        </div>
+        {sideBarData.sideBarIsOpen ? (
+          <div className="w-72 p-3 bg-gray-200 overflow-auto">
             {sideBarData.sideBarContent === 'ERRORS' ? (
               <ErrorPanel selectedPath={sideBarData.sideBarSelectedPath} />
             ) : sideBarData.sideBarContent === 'COMMENTS' ? (
@@ -66,8 +56,23 @@ export default function SideBar() {
               <InfoPanel selectedPath={sideBarData.sideBarSelectedPath} />
             ) : null}
           </div>
-        </div>
-      ) : null}
-    </>
+        ) : null}
+      </div>
+      <div
+        className="mt-auto h-12 w-full"
+        onClick={() => sideBarData.setSideBarIsOpen(!sideBarData.sideBarIsOpen)}
+      >
+        <button className="w-full h-full bg-gray-200 hover:bg-gray-100">
+          {sideBarData.sideBarIsOpen ? (
+            <>
+              <FontAwesomeIcon className="fa-xs" icon={faAngleDoubleRight} />{' '}
+              collapse Sidebar
+            </>
+          ) : (
+            <FontAwesomeIcon className="fa-xs" icon={faAngleDoubleLeft} />
+          )}
+        </button>
+      </div>
+    </div>
   )
 }
