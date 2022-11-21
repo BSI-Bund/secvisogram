14a9a50284be25c6f343de7497e3816bde7b0b72
vdeyringer <valentin.deyringer@exxcellent.de>
2022-11-21 17:51:57 +0100

    fix: resolve type errors
    remove overly complex MetaData type which is not required

--- app/lib/app/SecvisogramPage/View/WizardTab/shared/types.ts
+++ app/lib/app/SecvisogramPage/View/WizardTab/shared/types.ts
@@ -3,33 +3,17 @@ export type Property = Readonly<{
   title?: string
   type: 'STRING' | 'ARRAY' | 'OBJECT' | 'RECURSION'
   fullName: ReadonlyArray<string>
-  description: string
+  description?: string
   mandatory?: boolean
-  enum?: string[]
+  enum?: ReadonlyArray<string>
   pattern?: string
   minLength?: number
   addMenuItemsForChildObjects?: boolean
   metaInfo: MetaInfo
-  metaData?: MetaData
+  metaData?: any
 }>
 
 type MetaInfo = Readonly<{
   propertyList?: ReadonlyArray<Property>
   arrayType?: Property
-}>
-
-type MetaData = Readonly<{
-  uiType?:
-    | 'STRING_DATETIME'
-    | 'STRING_URI'
-    | 'STRING_ENUM'
-    | 'STRING_MULTI_LINE'
-    | 'STRING_PRODUCT_ID'
-    | 'STRING_GROUP_ID'
-    | 'OBJECT_CWE'
-    | 'OBJECT_CVSS_2'
-    | 'OBJECT_CVSS_3'
-    | 'ARRAY_REVISION_HISTORY'
-  options?: string[]
-  freeSolo?: boolean
 }>
