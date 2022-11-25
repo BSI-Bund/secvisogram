c3ba8814dc89a56416b1b5da905b7b850908784e
Christoph Löser <33527573+cloeser@users.noreply.github.com>
2022-11-25 17:02:09 +0100

    fix remove unused vars

--- app/lib/app/SecvisogramPage/View/WizardTab.js
+++ app/lib/app/SecvisogramPage/View/WizardTab.js
@@ -3,11 +3,9 @@ import { GenericEditor } from './WizardTab/editors.js'
 import schema from './WizardTab/schema.js'
 
 export default function WizardTab() {
-  const [selectedPath, setSelectedPath] = React.useState(
-    /** @type {string[]} */ ([])
+  const property = /** @type {import('./WizardTab/schema.js').Property} */ (
+    schema
   )
-
-  const property = /** @type {import('./WizardTab/schema.js').Property} */ (schema)
 
   return (
     <div className="flex h-full w-full grow">
