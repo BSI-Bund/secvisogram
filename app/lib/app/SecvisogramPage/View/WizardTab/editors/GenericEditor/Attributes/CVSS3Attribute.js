b89069ddf6261bd540c10279ba2598c73e936eb2
vdeyringer <valentin.deyringer@exxcellent.de>
2022-12-08 16:50:05 +0100

    refactor(CVSS3Attribute.js): add spacing around input fields

--- app/lib/app/SecvisogramPage/View/WizardTab/editors/GenericEditor/Attributes/CVSS3Attribute.js
+++ app/lib/app/SecvisogramPage/View/WizardTab/editors/GenericEditor/Attributes/CVSS3Attribute.js
@@ -67,191 +67,195 @@ export default function CVSSV3Attribute({
 
   return (
     <DocumentEditorContext.Provider value={documentEditor}>
-      <Attribute
-        label={label}
-        description={description}
-        instancePath={instancePath}
-      >
-        {dropdownFor('version', ['3.0', '3.1'])}
-        <TextAttribute
-          label="VectorString"
-          description=""
-          pattern="^CVSS:3.[01]/((AV:[NALP]|AC:[LH]|PR:[UNLH]|UI:[NR]|S:[UC]|[CIA]:[NLH]|E:[XUPFH]|RL:[XOTWU]|RC:[XURC]|[CIA]R:[XLMH]|MAV:[XNALP]|MAC:[XLH]|MPR:[XUNLH]|MUI:[XNR]|MS:[XUC]|M[CIA]:[XNLH])/)*(AV:[NALP]|AC:[LH]|PR:[UNLH]|UI:[NR]|S:[UC]|[CIA]:[NLH]|E:[XUPFH]|RL:[XOTWU]|RC:[XURC]|[CIA]R:[XLMH]|MAV:[XNALP]|MAC:[XLH]|MPR:[XUNLH]|MUI:[XNR]|MS:[XUC]|M[CIA]:[XNLH])$"
-          minLength={1}
-          required={true}
-          instancePath={instancePath.concat(['vectorString'])}
-          value={value?.vectorString || ''}
-        />
-        {canBeUpgraded ? (
-          <div className="mb-2">
-            <DefaultButton
-              onClick={() => {
-                const updatedCVSSMetrics = cvssVector.set('version', '3.1')
-                updateDoc(instancePath, updatedCVSSMetrics.data)
-              }}
-            >
-              Upgrade to CVSS 3.1
-            </DefaultButton>
-          </div>
-        ) : null}
-        {dropdownFor('attackVector', [
-          'NETWORK',
-          'ADJACENT_NETWORK',
-          'LOCAL',
-          'PHYSICAL',
-        ])}
-        {dropdownFor('attackComplexity', ['HIGH', 'LOW'])}
-        {dropdownFor('privilegesRequired', ['NONE', 'HIGH', 'LOW'])}
-        {dropdownFor('userInteraction', ['NONE', 'REQUIRED'])}
-        {dropdownFor('scope', ['UNCHANGED', 'CHANGED'])}
-        {dropdownFor('confidentialityImpact', ['NONE', 'HIGH', 'LOW'])}
-        {dropdownFor('integrityImpact', ['NONE', 'HIGH', 'LOW'])}
-        {dropdownFor('availabilityImpact', ['NONE', 'HIGH', 'LOW'])}
+      <div className="gap-4 p-4">
         <Attribute
-          label={'BaseScore'}
-          description={'The CVSS Base Score'}
-          instancePath={instancePath.concat(['baseScore'])}
+          label={label}
+          description={description}
+          instancePath={instancePath}
         >
-          {typeof value?.baseScore === 'number' ? String(value.baseScore) : ''}
+          {dropdownFor('version', ['3.0', '3.1'])}
+          <TextAttribute
+            label="VectorString"
+            description=""
+            pattern="^CVSS:3.[01]/((AV:[NALP]|AC:[LH]|PR:[UNLH]|UI:[NR]|S:[UC]|[CIA]:[NLH]|E:[XUPFH]|RL:[XOTWU]|RC:[XURC]|[CIA]R:[XLMH]|MAV:[XNALP]|MAC:[XLH]|MPR:[XUNLH]|MUI:[XNR]|MS:[XUC]|M[CIA]:[XNLH])/)*(AV:[NALP]|AC:[LH]|PR:[UNLH]|UI:[NR]|S:[UC]|[CIA]:[NLH]|E:[XUPFH]|RL:[XOTWU]|RC:[XURC]|[CIA]R:[XLMH]|MAV:[XNALP]|MAC:[XLH]|MPR:[XUNLH]|MUI:[XNR]|MS:[XUC]|M[CIA]:[XNLH])$"
+            minLength={1}
+            required={true}
+            instancePath={instancePath.concat(['vectorString'])}
+            value={value?.vectorString || ''}
+          />
+          {canBeUpgraded ? (
+            <div className="mb-2">
+              <DefaultButton
+                onClick={() => {
+                  const updatedCVSSMetrics = cvssVector.set('version', '3.1')
+                  updateDoc(instancePath, updatedCVSSMetrics.data)
+                }}
+              >
+                Upgrade to CVSS 3.1
+              </DefaultButton>
+            </div>
+          ) : null}
+          {dropdownFor('attackVector', [
+            'NETWORK',
+            'ADJACENT_NETWORK',
+            'LOCAL',
+            'PHYSICAL',
+          ])}
+          {dropdownFor('attackComplexity', ['HIGH', 'LOW'])}
+          {dropdownFor('privilegesRequired', ['NONE', 'HIGH', 'LOW'])}
+          {dropdownFor('userInteraction', ['NONE', 'REQUIRED'])}
+          {dropdownFor('scope', ['UNCHANGED', 'CHANGED'])}
+          {dropdownFor('confidentialityImpact', ['NONE', 'HIGH', 'LOW'])}
+          {dropdownFor('integrityImpact', ['NONE', 'HIGH', 'LOW'])}
+          {dropdownFor('availabilityImpact', ['NONE', 'HIGH', 'LOW'])}
+          <Attribute
+            label={'BaseScore'}
+            description={'The CVSS Base Score'}
+            instancePath={instancePath.concat(['baseScore'])}
+          >
+            {typeof value?.baseScore === 'number'
+              ? String(value.baseScore)
+              : ''}
+          </Attribute>
+          <Attribute
+            label={'BaseSeverity'}
+            description={'The CVSS Base Severity'}
+            instancePath={instancePath.concat(['baseSeverity'])}
+          >
+            {value?.baseSeverity || ''}
+          </Attribute>
+          {dropdownFor('exploitCodeMaturity', [
+            'UNPROVEN',
+            'PROOF_OF_CONCEPT',
+            'FUNCTIONAL',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('remediationLevel', [
+            'OFFICIAL_FIX',
+            'TEMPORARY_FIX',
+            'WORKAROUND',
+            'UNAVAILABLE',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('reportConfidence', [
+            'UNKNOWN',
+            'REASONABLE',
+            'CONFIRMED',
+            'NOT_DEFINED',
+            '',
+          ])}
+          <Attribute
+            label={'TemporalScore'}
+            description={'The CVSS Temporal Score'}
+            instancePath={instancePath.concat(['temporalScore'])}
+          >
+            {typeof value?.temporalScore === 'number'
+              ? String(value.temporalScore)
+              : ''}
+          </Attribute>
+          <Attribute
+            label={'TemporalSeverity'}
+            description={'The CVSS Temporal Severity'}
+            instancePath={instancePath.concat(['temporalSeverity'])}
+          >
+            {value?.temporalSeverity || ''}
+          </Attribute>
+          {dropdownFor('confidentialityRequirement', [
+            'LOW',
+            'MEDIUM',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('integrityRequirement', [
+            'LOW',
+            'MEDIUM',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('availabilityRequirement', [
+            'LOW',
+            'MEDIUM',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedAttackVector', [
+            'NETWORK',
+            'ADJACENT_NETWORK',
+            'LOCAL',
+            'PHYSICAL',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedAttackComplexity', [
+            'HIGH',
+            'LOW',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedPrivilegesRequired', [
+            'NONE',
+            'LOW',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedUserInteraction', [
+            'NONE',
+            'REQUIRED',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedScope', [
+            'UNCHANGED',
+            'CHANGED',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedConfidentialityImpact', [
+            'NONE',
+            'LOW',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedIntegrityImpact', [
+            'NONE',
+            'LOW',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          {dropdownFor('modifiedAvailabilityImpact', [
+            'NONE',
+            'LOW',
+            'HIGH',
+            'NOT_DEFINED',
+            '',
+          ])}
+          <Attribute
+            label={'EnvironmentalScore'}
+            description={'The CVSS Environmental Score'}
+            instancePath={instancePath.concat(['environmentalScore'])}
+          >
+            {typeof value?.environmentalScore === 'number'
+              ? String(value.environmentalScore)
+              : ''}
+          </Attribute>
+          <Attribute
+            label={'EnvironmentalSeverity'}
+            description={'The CVSS Environmental Severity'}
+            instancePath={instancePath.concat(['environmentalSeverity'])}
+          >
+            {value?.environmentalSeverity || ''}
+          </Attribute>
         </Attribute>
-        <Attribute
-          label={'BaseSeverity'}
-          description={'The CVSS Base Severity'}
-          instancePath={instancePath.concat(['baseSeverity'])}
-        >
-          {value?.baseSeverity || ''}
-        </Attribute>
-        {dropdownFor('exploitCodeMaturity', [
-          'UNPROVEN',
-          'PROOF_OF_CONCEPT',
-          'FUNCTIONAL',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('remediationLevel', [
-          'OFFICIAL_FIX',
-          'TEMPORARY_FIX',
-          'WORKAROUND',
-          'UNAVAILABLE',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('reportConfidence', [
-          'UNKNOWN',
-          'REASONABLE',
-          'CONFIRMED',
-          'NOT_DEFINED',
-          '',
-        ])}
-        <Attribute
-          label={'TemporalScore'}
-          description={'The CVSS Temporal Score'}
-          instancePath={instancePath.concat(['temporalScore'])}
-        >
-          {typeof value?.temporalScore === 'number'
-            ? String(value.temporalScore)
-            : ''}
-        </Attribute>
-        <Attribute
-          label={'TemporalSeverity'}
-          description={'The CVSS Temporal Severity'}
-          instancePath={instancePath.concat(['temporalSeverity'])}
-        >
-          {value?.temporalSeverity || ''}
-        </Attribute>
-        {dropdownFor('confidentialityRequirement', [
-          'LOW',
-          'MEDIUM',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('integrityRequirement', [
-          'LOW',
-          'MEDIUM',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('availabilityRequirement', [
-          'LOW',
-          'MEDIUM',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedAttackVector', [
-          'NETWORK',
-          'ADJACENT_NETWORK',
-          'LOCAL',
-          'PHYSICAL',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedAttackComplexity', [
-          'HIGH',
-          'LOW',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedPrivilegesRequired', [
-          'NONE',
-          'LOW',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedUserInteraction', [
-          'NONE',
-          'REQUIRED',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedScope', [
-          'UNCHANGED',
-          'CHANGED',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedConfidentialityImpact', [
-          'NONE',
-          'LOW',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedIntegrityImpact', [
-          'NONE',
-          'LOW',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        {dropdownFor('modifiedAvailabilityImpact', [
-          'NONE',
-          'LOW',
-          'HIGH',
-          'NOT_DEFINED',
-          '',
-        ])}
-        <Attribute
-          label={'EnvironmentalScore'}
-          description={'The CVSS Environmental Score'}
-          instancePath={instancePath.concat(['environmentalScore'])}
-        >
-          {typeof value?.environmentalScore === 'number'
-            ? String(value.environmentalScore)
-            : ''}
-        </Attribute>
-        <Attribute
-          label={'EnvironmentalSeverity'}
-          description={'The CVSS Environmental Severity'}
-          instancePath={instancePath.concat(['environmentalSeverity'])}
-        >
-          {value?.environmentalSeverity || ''}
-        </Attribute>
-      </Attribute>
+      </div>
     </DocumentEditorContext.Provider>
   )
 }
