f13f3b6a4c03ef2211bf6a06ada98b7af71e794e
vdeyringer <valentin.deyringer@exxcellent.de>
2023-03-13 09:31:37 +0100

    fix(previewTab.js): show scrollbar
    scrollbar disappeared when showing the preview tab

--- app/lib/app/SecvisogramPage/View/PreviewTab.js
+++ app/lib/app/SecvisogramPage/View/PreviewTab.js
@@ -53,7 +53,6 @@ export default function PreviewTab({ onPreview, previewResult }) {
               className="advisory w-full border h-full"
               ref={iframeRef}
             />
-            <div className="absolute top-0 right-0 bottom-0 left-0"></div>
           </div>
         ) : (
           <div className="relative h-full">
